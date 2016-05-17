0c0eb41e6956f0bf2924c18f9914c05c75c74bb9
Bahadir Cambel <bcambel@gmail.com>
2016-05-17 12:07:36 +0200

    bump dep versions

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-          [org.clojure/clojure "1.7.0"]
+          [org.clojure/clojure "1.8.0"]
           [org.clojure/core.async "0.2.374"]
           [org.clojure/tools.cli "0.3.1"]
-          [com.stuartsierra/component "0.3.0"]
-          [compojure "1.4.0"]
+          [com.stuartsierra/component "0.3.1"]
+          [compojure "1.5.0"]
           [ring "1.4.0"]
-          [com.taoensso/timbre "4.1.4"]
-          [cheshire "5.5.0"]
-          [amazonica "0.3.39"]
+          [com.taoensso/timbre "4.3.1"]
+          [cheshire "5.6.0"]
+          [amazonica "0.3.57"]
           [metrics-clojure "2.4.0"]
           [byte-streams "0.2.0"]
           ; [com.taoensso/nippy "2.7.0"]
